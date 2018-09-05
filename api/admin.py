e69452501b710fb9864fcf997eaf6ffe8cad9495
Riley Mathews <rileymathews80@gmail.com>
2018-09-05 10:34:23 -0500

    registered models with the admin site

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from api.models import User
+from api.models import User, Lesson, StudentLesson, Student, Subject, Teacher, TeacherStudent
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Lesson)
+admin.site.register(StudentLesson)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Teacher)
+admin.site.register(TeacherStudent)
