fdc84d88c4fea4c8b9b0238cb7fbce946654ab62
Riley Mathews <rileymathews80@gmail.com>
2018-09-06 12:53:02 -0500

    changed zip code to char field

--- api/models/teacher.py
+++ api/models/teacher.py
@@ -8,7 +8,7 @@ class Teacher(models.Model):
     city = models.CharField(max_length=255, blank=True)
     region = models.CharField(max_length=255, blank=True)
     country = models.CharField(max_length=255, blank=True)
-    zip_code = models.IntegerField(blank=True, null=True)
+    zip_code = models.CharField(max_length=15, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     students = models.ManyToManyField(Student, through='TeacherStudent')
 
