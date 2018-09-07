6279025f008322f51ba7efecf88041119f10ab96
Riley Mathews <rileymathews80@gmail.com>
2018-09-07 13:19:50 -0500

    assignments now filter based on the current user

--- api/views/student_lesson_view.py
+++ api/views/student_lesson_view.py
@@ -5,3 +5,17 @@ from api.serializers import StudentLessonSerializer
 class StudentLessonView(viewsets.ModelViewSet):
     queryset = StudentLesson.objects.all()
     serializer_class = StudentLessonSerializer
+
+    def get_queryset(self):
+        current_user = self.request.user
+        if current_user.is_teacher:
+            print("teacher")
+            queryset = StudentLesson.objects.filter(teacher=current_user.teacher)
+        elif current_user.is_student:
+            print("student")
+            print(current_user.student)
+            queryset = StudentLesson.objects.filter(student=current_user.student)
+        else:
+            queryset = StudentLesson.objects.all()
+
+        return queryset
