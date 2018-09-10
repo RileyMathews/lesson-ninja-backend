12f5808f67c6de754901a41f2d475e0b1ac17c96
Riley Mathews <rileymathews80@gmail.com>
2018-09-10 11:41:28 -0500

    added embeded info on lesson serializer

--- api/serializers/lesson_serializer.py
+++ api/serializers/lesson_serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from api.models import Lesson
 from .teacher_serializer import TeacherSerializer
+from .document_serializer import DocumentSerializer
 
 class LessonSerializer(serializers.HyperlinkedModelSerializer):
     teacher = TeacherSerializer(read_only=True)
+    documents = DocumentSerializer(many=True, read_only=True)
     class Meta:
         model = Lesson
         fields = (
@@ -12,5 +14,6 @@ class LessonSerializer(serializers.HyperlinkedModelSerializer):
             'name',
             'description',
             'content',
-            'teacher'
+            'teacher',
+            'documents'
         )
