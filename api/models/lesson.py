407b9023086ea005b453defedb4a717132f03c34
Riley Mathews <rileymathews80@gmail.com>
2018-09-10 11:39:21 -0500

    added many to many field on lesson

--- api/models/lesson.py
+++ api/models/lesson.py
@@ -1,11 +1,13 @@
 from django.db import models
 from .teacher import Teacher
+from .document import Document
 
 class Lesson(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     content = models.CharField(max_length=10000)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    documents = models.ManyToManyField(Document, through='LessonDocument')
 
     def __str__(self):
         return self.name
