5abd2920f65058516aa3e3646027b8f46a32963d
Riley Mathews <rileymathews80@gmail.com>
2018-09-10 09:11:02 -0500

    added field to model for teacher

--- api/models/document.py
+++ api/models/document.py
@@ -1,6 +1,8 @@
 from django.db import models
+from .teacher import Teacher
 
 class Document(models.Model):
     name = models.CharField(max_length=255)
     notes = models.CharField(max_length=255)
     s3_url = models.CharField(max_length=500)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
