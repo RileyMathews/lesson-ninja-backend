0223ad017b33ac7e0f2a3efda2d7da5066791cd6
Riley Mathews <rileymathews80@gmail.com>
2018-09-10 09:49:33 -0500

    changed serializer for document

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import User, Lesson, StudentLesson, Student, Subject, Teacher, TeacherStudent
+from api.models import User, Lesson, StudentLesson, Student, Subject, Teacher, TeacherStudent, Document
 
 # Register your models here.
 admin.site.register(User)
@@ -9,3 +9,4 @@ admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(Teacher)
 admin.site.register(TeacherStudent)
+admin.site.register(Document)
--- api/serializers/document_serializer.py
+++ api/serializers/document_serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from api.models import Document
+from .teacher_serializer import TeacherSerializer
 
 class DocumentSerializer(serializers.HyperlinkedModelSerializer):
+    teacher = TeacherSerializer(read_only=True)
     class Meta:
         model = Document
         fields = (
