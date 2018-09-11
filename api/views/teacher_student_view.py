aacc1c3d3bb60e29883263b11cd3fe8fe5aba497
Riley Mathews <rileymathews80@gmail.com>
2018-09-11 11:26:11 -0500

    edited intersection

--- api/views/teacher_student_view.py
+++ api/views/teacher_student_view.py
@@ -5,3 +5,14 @@ from api.serializers import TeacherStudentSerializer
 class TeacherStudentView(viewsets.ModelViewSet):
     queryset = TeacherStudent.objects.all()
     serializer_class = TeacherStudentSerializer
+
+    def get_queryset(self):
+        queryset = TeacherStudent.objects.all()
+        # get url parameters
+        teacher = self.request.query_params.get("teacher", None)
+        student = self.request.query_params.get("student", None)
+        # filter off those parameters
+        if teacher and student:
+            queryset = [item for item in queryset if item.teacher.id == int(teacher) and item.student.id == int(student)]
+        # return queryset
+        return queryset
