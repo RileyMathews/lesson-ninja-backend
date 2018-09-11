c39c7feb8898b61a7b7a62bc2e27d7be800e1e1a
Riley Mathews <rileymathews80@gmail.com>
2018-09-11 10:59:58 -0500

    added filter queryset to intersection table for lesson document

--- api/views/lesson_document_view.py
+++ api/views/lesson_document_view.py
@@ -5,3 +5,13 @@ from api.serializers import LessonDocumentSerializer
 class LessonDocumentView(viewsets.ModelViewSet):
     queryset = LessonDocument.objects.all()
     serializer_class = LessonDocumentSerializer
+
+    def get_queryset(self):
+        queryset = LessonDocument.objects.all()
+        # get url parameters
+        lesson = self.request.query_params.get("lesson", None)
+        document = self.request.query_params.get("document", None)
+        # filter off those parameters
+        queryset = [item for item in queryset if item.lesson.id == int(lesson) and item.document.id == int(document)]
+        # return queryset
+        return queryset
