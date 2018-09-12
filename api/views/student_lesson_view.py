6418ab04f4839a3d1d2b6de86e6ae13d294936eb
Riley Mathews <rileymathews80@gmail.com>
2018-09-12 09:05:20 -0500

    added parameter query to assignment endpoint to prevent bug

--- api/views/student_lesson_view.py
+++ api/views/student_lesson_view.py
@@ -8,19 +8,19 @@ class StudentLessonView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         current_user = self.request.user
-        if current_user.is_teacher:
-            print(current_user.teacher)
-            teachers_lessons = current_user.teacher.lesson_set.all()
-            print(teachers_lessons)
-            queryset = StudentLesson.objects.all()
-            queryset = [assignment for assignment in queryset if assignment.lesson in teachers_lessons]
-        elif current_user.is_student:
-            print("student")
-            print(current_user.student)
-            queryset = StudentLesson.objects.filter(student=current_user.student)
-        else:
-            queryset = StudentLesson.objects.all()
-
+        queryset = StudentLesson.objects.all()
+        if self.request.query_params.get("users", None):
+            if current_user.is_teacher:
+                print(current_user.teacher)
+                teachers_lessons = current_user.teacher.lesson_set.all()
+                print(teachers_lessons)
+                queryset = StudentLesson.objects.all()
+                queryset = [assignment for assignment in queryset if assignment.lesson in teachers_lessons]
+            elif current_user.is_student:
+                print("student")
+                print(current_user.student)
+                queryset = StudentLesson.objects.filter(student=current_user.student)
+                
         return queryset
 
     def get_serializer_class(self):
