8e6cf76dd910429016eacbebfd7cc290c2cf0e1f
Riley Mathews <rileymathews80@gmail.com>
2018-09-13 17:05:56 -0500

    modified settings structure to automatically change on deployment

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -19,11 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&mszoidtgbj&d$j(j*+%d(4s=1$nou5him6+p#!$d=07*8$0o$'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["142.93.23.116", "127.0.0.1"]
 
---
+++ backend/settings/production.py
@@ -0,0 +1,6 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+CORS_ORIGIN_WHITELIST = ('lesson.ninja')
