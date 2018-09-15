80b046f28ac992dc500555ab6d8efca32f74ed1f
Riley Mathews <rileymathews80@gmail.com>
2018-09-15 15:29:48 -0500

    changed production database settings

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -13,7 +13,11 @@ ALLOWED_HOSTS = ["api.lesson.ninja"]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lessonninja',
+        'USER': 'rileymathews',
+        'PASSWORD': 'nerdrage132',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
