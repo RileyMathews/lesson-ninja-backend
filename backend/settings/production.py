c0505b8b47cdd256b4ac0f901f71bdf3633dc1c8
Riley Mathews <rileymathews80@gmail.com>
2018-09-17 20:34:51 -0500

    added full domain to cors whitelist

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CORS_ORIGIN_WHITELIST = ('lesson.ninja')
+CORS_ORIGIN_WHITELIST = ('lesson.ninja', 'https://lesson.ninja')
 
 ALLOWED_HOSTS = ["api.lesson.ninja"]
 
